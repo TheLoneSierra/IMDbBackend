5efabb578b6e6f7001791b4e8816337123f90287
Aanchal Pal <aanchalpal2009@gmail.com>
2024-08-31 21:42:28 +0530

    changes made

--- index.js
+++ index.js
@@ -13,8 +13,8 @@ const app = express();
 
 // Use CORS middleware
 app.use(cors({
-    origin: ['http://localhost:5173'], // Add your frontend's local URL here
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS', // Include OPTIONS
+    origin: ['https://imdb-frontend-kappa.vercel.app/'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true
 }));
 
